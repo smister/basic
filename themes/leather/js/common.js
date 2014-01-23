214d6783af2dbc254a951c3ad341a6511fc9b3c7
Jingye Wang <4893278@qq.com>
2014-01-23 14:29:06 +0800

    Signed-off-by: Jingye Wang <4893278@qq.com>

--- themes/leather/js/common.js
+++ themes/leather/js/common.js
@@ -29,26 +29,42 @@ $(document).ready(function () {
             $("#cartForm").submit();
         }
     });
-    $("#cart-table").on("change", "input[type=text][id=quantity]", function () {
-        var tr = $(this).closest('tr');
-        var sku_id = tr.find("#position");
-        var qty = tr.find("#quantity");
-        var item_id = tr.find(".item-id");
-        var props = tr.find(".props");
-        var cart=parseInt($(".shopping_car").find("span").html());
-       var sumPrice= parseFloat($("#SumPrice").html());
-        var singlePrice=parseFloat( tr.find("#Singel-Price").html());
+    $("#cart-table").on("keyup", "input[type=text][id=quantity]", function () {
+
+        var $this = $(this),
+            $tr = $this.closest('tr'),
+            props = $tr.find('.props'),
+            tempId = $tr.find('.item-id'),
+            qty = $.trim(this.value);
+        clearTimeout(window.delay);
+        window.delay = setTimeout(function () {
+            $this.blur();
+            // check input data
             if (!/^\d+$/.test(qty)) {
                 return;
             }
-        $.get($(this).data('url'), {'sku_id': sku_id.val(), 'qty': qty.val(), 'item_id': item_id.val(), 'props': props.val()}, function (response) {
-            $("#error-message").remove();
-            if (!response) {
-                $(".shopping_car").find("span").html(cart-sumPrice/singlePrice+parseInt(qty.val()));
-                tr.find("#SumPrice").html(parseFloat(qty.val()) * parseFloat(singlePrice));
+            var html = '<input type="hidden" name="hid" value="0">';
+            // compare number
+            if (parseInt(qty) <= parseInt($this.data('num'))) {
+                $.post($(this).data('url'), {'item_id': tempId, 'props': props, 'qty': qty}, function (response) {
+//             window.location.reload();
+                    $("id").attr("value");
+                }, 'json');
+            } else {
+                var s = "库存不足，请更改物品数量！";
+                document.write(s);
+//                show error
             }
-            $("#stock-error").append(response);
-        });
+        }, 1500);
+    });
+    $('[name="quantity[]"]').change(function () {
+        var item_id = $(this).parents('tr').find('[name="item_id[]"]').val();
+        var props = $(this).parents('tr').find('[name="props[]"]').val();
+        var qty = $(this).val();
+        var data = {'item_id': item_id, 'props': props, 'qty': qty};
+        $.post($(this).data('url'), data, function (response) {
+            window.location.reload();
+        }, 'json');
     });
     $('#cartForm').on('click', '[name="position[]"],#checkAllPosition', function () {
         var flag = 0;
@@ -62,9 +78,9 @@ $(document).ready(function () {
             }
         } else {
             $('#checkAllPosition').removeAttr('checked');
-            if ($(this).attr("checked")) {
-                $(this).attr("checked", "checked");
-            } else {
+            if($(this).attr("checked")){
+                $(this).attr("checked","checked");
+            }else{
                 $(this).removeAttr("checked");
                 flag = 0;
             }
