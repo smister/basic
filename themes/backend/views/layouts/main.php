4633a6c1170bee35d9ef46437c62b3cc5b95e35b
Jingye Wang <4893278@qq.com>
2014-02-11 15:48:57 +0800

    Signed-off-by: Jingye Wang <4893278@qq.com>

--- themes/backend/views/layouts/main.php
+++ themes/backend/views/layouts/main.php
@@ -21,31 +21,11 @@
         array(
             'class' => 'bootstrap.widgets.TbNav',
             'items' => array(
-                array('label' => '控制面板', 'url' => array('/site/index'), 'visible' => !Yii::app()->user->isGuest),
-                array('label' => '内容管理', 'url' => '#', 'items' => array(
-                    array('label' => '内容分类', 'url' => array('/cms/contentCategory/admin')),
-                    array('label' => '单页管理', 'url' => array('/cms/page/admin')),
-                    array('label' => '文章管理', 'url' => array('/cms/article/admin')),
-                    array('label' => '留言管理', 'url' => array('/cms/feedback/admin')),
-                    array('label' => 'OTHER'),
-                    array('label' => '广告管理', 'url' => array('/cms/ad/admin')),
-                    array('label' => '友情链接', 'url' => array('/cms/friendLink/admin')),
-                    array('label' => '邮件订阅', 'url' => array('/cms/newsletterSubscriber/admin')),
-                    array('label' => '客服管理', 'url' => array('/cms/customerService/admin')),
-                ), 'visible' => !Yii::app()->user->isGuest),
                 array('label' => '商城管理', 'url' => '#', 'items' => array(
                     array('label' => 'ITEM'),
-                    array('label' => '商品分类', 'url' => array('/mall/itemCategory/admin')),
                     array('label' => '商品属性', 'url' => array('/mall/itemProp/admin')),
                     array('label' => '商品列表', 'url' => array('/mall/item/admin')),
                     array('label' => '图片空间', 'url' => array('/mall/elfinder/admin')),
-                    array('label' => 'Payment'),
-                    array('label' => '支付方式', 'url' => array('/mall/paymentMethod/admin')),
-                    array('label' => '物流配送', 'url' => array('/mall/shippingMethod/admin')),
-                    array('label' => 'Order'),
-                    array('label' => '订单列表', 'url' => array('/mall/order/admin')),
-                    array('label' => '资金列表', 'url' => array('/mall/payment/admin')),
-                    array('label' => '订单日志', 'url' => array('/mall/orderLog/admin')),
                     array('label' => 'Service'),
                     array('label' => '发货单', 'url' => array('/mall/shipping/admin')),
                     array('label' => '退货单', 'url' => array('/mall/refund/admin')),
@@ -56,13 +36,6 @@
                     array('label' => '权限管理', 'url' => array('/auth/assignment/index')),
                 ), 'visible' => !Yii::app()->user->isGuest),
                 array('label' => '插件列表', 'url' => array('/plugin/pluginmanage/index'), 'visible' => !Yii::app()->user->isGuest),
-                array('label' => '系统设置', 'url' => '#', 'items' => array(
-                    array('label' => '菜单管理', 'url' => array('/menu/admin')),
-                    array('label' => '分类管理', 'url' => array('/category/admin')),
-                    array('label' => '皮肤管理', 'url' => array('/theme/admin')),
-                    array('label' => '语言管理', 'url' => array('/language/admin')),
-                    array('label' => '货币管理', 'url' => array('/currency/admin')),
-                ), 'visible' => !Yii::app()->user->isGuest),
             ),
         ),
 
@@ -84,7 +57,7 @@
 
 <div class="container-fluid" id="page">
 
-    <?php  echo $content; ?>
+    <?php echo $content; ?>
 
     <div class="clear"></div>
 
